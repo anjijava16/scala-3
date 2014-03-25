98e0c2cbec0e4a2d801083d67f2454fd456ad02f
Po Liu <po.liu@hootsuite.com>
2014-03-24 23:52:38 -0700

    list built in return type added

---
+++ 99-problems/p3.scala
@@ -0,0 +1,11 @@
+object p1 {
+  def main(args: Array[String]): Unit = {
+    val myList = List(1, 1, 2, 3, 5, 8)
+    
+    println(nth(2, myList))
+  }
+
+  def nth(query: Int, input: List[Int]): Int = {
+    input(query)
+  }
+}
