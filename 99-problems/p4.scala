5af489cb13c8fab88613d1f044a3ea8d818b8141
Po Liu <po.liu@hootsuite.com>
2014-03-25 00:00:40 -0700

    length is built in too

---
+++ 99-problems/p4.scala
@@ -0,0 +1,11 @@
+object p1 {
+  def main(args: Array[String]): Unit = {
+    val myList = List(1, 1, 2, 3, 5, 8)
+    
+    println(length(myList))
+  }
+
+  def length(input: List[Int]): Int = {
+	  input.length
+  }
+}
