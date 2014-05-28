df08ab1dbc38db651155d0589e7dc1e95ff36d1b
Po Liu <po.liu@hootsuite.com>
2014-05-27 18:42:23 -0700

    Flattening lists

---
+++ 99-problems/p7.scala
@@ -0,0 +1,13 @@
+object p6 {
+  def main(args: Array[String]): Unit = {
+    val result = flatten(List(List(1,1), 2, List(3, List(5, 8))))
+
+    println(result.toString)
+  }
+
+  def flatten(input:List[_]): List[Int] = input flatMap {
+    case i: List[_] => flatten(i)
+    case e:Int => List(e)
+  }
+
+}
