5a81482b13ad8873230a68387f4d4d28acf23bc4
Po Liu <po.liu@hootsuite.com>
2014-06-21 11:41:30 -0700

    Solution to counting change problem

--- coursera/recfun/src/main/scala/recfun/Main.scala
+++ coursera/recfun/src/main/scala/recfun/Main.scala
@@ -65,6 +65,20 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    1
+      if (money == 0) {
+        1
+      }
+      else if (money < 0) {
+        0
+      }
+      else if (coins.isEmpty) {
+        0
+      }
+      else {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      }
+
   }
+
+
 }
